66760e47e2a6f741e04c7e97de83b55518752916
Javidan Abdullayev <75013734+Javidan1997@users.noreply.github.com>
2021-12-03 17:12:49 +0400

    Update rabitabank.sql

--- Reports_MA/rabitabank.sql
+++ Reports_MA/rabitabank.sql
@@ -1,26 +1,14 @@
-select   [AgentPaymentID]
-		,[ServiceName]
-		,[MONTH]
-		,[AgentTerminalID]
-		,SUM([PaySum]) Sum
-		,Count(*) Count
-		,sessionid from 
-(SELECT 
-		[AgentPaymentID]  
+SELECT 	[AgentPaymentID]  
 		,s.[ServiceName]
 		,MONTH(StatusDate) Month
 		,[Number]
 		,[AgentTerminalID]
 		,[PaySum]
+		,ExtraParams
 		,CAST([ExtraParams].query('data(r/sessionid)') as nvarchar) as sessionid
+		,CAST([ExtraParams].query('data(r/rabita_session)') as nvarchar) as rabita_session
   FROM [dbo].[Payment] p 
   JOIN Service s ON s.ServiceID=p.ServiceID
 	WHERE s.ProviderID in (63) and Status=2 
-	and StatusDate between '2021-06-01' and '2021-06-02' 
+	and StatusDate between '2021-11-01' and '2021-12-01' 
 	and CAST([ExtraParams].query('data(r/agt_id)') as nvarchar) <> '3'
-	) a 
-	GROUP BY MONTH
-		,[AgentPaymentID]
-		,[ServiceName]
-		,[AgentTerminalID]
-		,[sessionid];
