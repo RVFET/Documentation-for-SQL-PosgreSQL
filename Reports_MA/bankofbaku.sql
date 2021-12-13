fe4c2a8c4779e25256b176cff2b93bf36f2fc0b7
Javidan Abdullayev <75013734+Javidan1997@users.noreply.github.com>
2021-12-13 16:23:47 +0400

    Update bankofbaku.sql

--- Reports_MA/bankofbaku.sql
+++ Reports_MA/bankofbaku.sql
@@ -3,8 +3,9 @@ PaymentID,AgentPaymentID,Number,PaySum,ProviderSum,StatusDate,
 CAST([PaymentInfo].query('data(r/transactionid)') as nvarchar) transactionid ,
 CAST([PaymentInfo].query('data(r/providerpaymentid)') as nvarchar) providerpaymentid ,
 CAST([PaymentInfo].query('data(r/batchid)') as nvarchar) batchid
+,ExtraParams
 
-FROM gate211.dbo.Payment WHERE ServiceID in (323,324,325,326,556)
+FROM gate211.dbo.Payment WHERE ServiceID in (323,324,325,326,556,1123)
  --(323,324,325)
 
-and (StatusDate between '2021-11-01' and '2021-12-01') and Status =2
+and (StatusDate between '2021-11-01' and '2021-11-02') and Status =2
