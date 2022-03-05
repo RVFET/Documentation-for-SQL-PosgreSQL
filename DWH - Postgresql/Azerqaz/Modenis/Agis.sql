2aa343c69e603c7c68e04c310e653c5fb4b05a4e
Javidan Abdullayev <75013734+Javidan1997@users.noreply.github.com>
2022-03-05 11:34:30 +0400

    Update Agis.sql

--- DWH - Postgresql/Azerqaz/Modenis/Agis.sql
+++ DWH - Postgresql/Azerqaz/Modenis/Agis.sql
@@ -1,3 +1,5 @@
+----Create-----
+
 SELECT --top 120-- AzerQazDirect 
 p.OsmpProviderID,
 p.AgentPaymentID as AzeriQazID, p.ReceiveDate, p."Number", p.PaySum, p.Status
@@ -6,3 +8,12 @@ p.AgentPaymentID as AzeriQazID, p.ReceiveDate, p."Number", p.PaySum, p.Status
 where  ServiceID = 275 and 
   StatusDate Between '2022-02-01'and'2022-03-01'
     order by agentdate
+    
+    
+-----Find difference--------
+
+SELECT *
+
+  FROM   test.agis_azerqaz a
+  full join test.agis_modenis m on cast (a.trn as varchar) = cast (m.azeriqazid as varchar)
+where  a.trn  is null and m.status = 2 or m.azeriqazid is null and m.status = 2
